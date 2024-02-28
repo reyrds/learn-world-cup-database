5f7eaca3b42eb390f5139ad43f07bfaebbc84506
Rey Racaza <racazareyn@gmail.com>
2024-02-28 01:18:25 +0000

    Save progress: 1.1

--- queries.sh
+++ queries.sh
@@ -29,7 +29,7 @@ echo -e "\nWinner of the 2018 tournament team name:"
 echo "$($PSQL "SELECT name FROM games LEFT JOIN teams ON games.winner_id=teams.team_id WHERE year = 2018 AND round = 'Final'")"
 
 echo -e "\nList of teams who played in the 2014 'Eighth-Final' round:"
-echo "$($PSQL "")"
+echo "$($PSQL "SELECT DISTINCT(name) FROM teams INNER JOIN games ON teams.team_id = games.winner_id OR teams.team_id = games.opponent_id WHERE round = 'Eighth-Final' AND year = 2014")"
 
 echo -e "\nList of unique winning team names in the whole data set:"
 echo "$($PSQL "SELECT name FROM games LEFT JOIN teams ON games.winner_id=teams.team_id GROUP BY name ORDER BY name")"
