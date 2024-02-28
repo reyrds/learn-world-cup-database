b3bfed48ddc09ab958cd43b9416f988ad43f5f83
Rey Racaza <racazareyn@gmail.com>
2024-02-28 01:07:48 +0000

    cont3

--- queries.sh
+++ queries.sh
@@ -29,13 +29,13 @@ echo -e "\nWinner of the 2018 tournament team name:"
 echo "$($PSQL "SELECT name FROM games LEFT JOIN teams ON games.winner_id=teams.team_id WHERE year = 2018 AND round = 'Final'")"
 
 echo -e "\nList of teams who played in the 2014 'Eighth-Final' round:"
-echo 
+echo "$($PSQL "")"
 
 echo -e "\nList of unique winning team names in the whole data set:"
-echo
+echo "$($PSQL "SELECT name FROM games LEFT JOIN teams ON games.winner_id=teams.team_id GROUP BY name ORDER BY name")"
 
 echo -e "\nYear and team name of all the champions:"
-echo
+echo "$($PSQL "SELECT year, name FROM games LEFT JOIN teams ON games.winner_id=teams.team_id WHERE round='Final' AND winner_goals > opponent_goals ORDER BY year ")"
 
 echo -e "\nList of teams that start with 'Co':"
-echo
+echo "$($PSQL "SELECT name FROM teams WHERE name LIKE 'Co%'")"
